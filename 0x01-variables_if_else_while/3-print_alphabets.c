0e37773b25c21f3cebf6811ebc133bd2efd6983c
poto17 <trpoto@gmail.com>
2023-07-16 11:30:31 -0700

    You've got this

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQESTUVWXYZ";
+		int i;
+
+		for(i = 0 < 52; i++)
+		{
+			putchar(alp[i]);
+		}
+		putchar("\n");
+		return (0);
+}
